7862d35981bd6f0e7b24398607164d91f6bf2982
Yana Kalsina <yaprogrammer18@gmail.com>
2026-02-28 20:59:07 +0300

    Add tests and som files for tests

--- src/CSVprettyPrinter/csvPrettyPrinter.c
+++ src/CSVprettyPrinter/csvPrettyPrinter.c
@@ -10,7 +10,7 @@
         по длине строки  в input.csv - 256
         по длине 1 слова в input.csv - 99
 В input.csv данные записаны корректно и без ошибок
-отсутствие пустых яеек (,,) в заголовках
+отсутствие пустых ячеек ",," в заголовках
 */
 
 int readFile(char** data, const char* filepath)
@@ -27,7 +27,7 @@ int readFile(char** data, const char* filepath)
         if (indexOfLineBreak < sizeof(temporaryBuffer)) {
             temporaryBuffer[indexOfLineBreak] = '\0';
         } else {
-            temporaryBuffer[sizeof(temporaryBuffer) - 1] = '\0'; // Безопасное значение
+            temporaryBuffer[sizeof(temporaryBuffer) - 1] = '\0';
         }
         char* buffer = malloc(strlen(temporaryBuffer) + 1);
         if (buffer == NULL) {
@@ -244,7 +244,6 @@ bool algorithm(const char* filepathInput, const char* filepathOutput)
         return false;
     }
     getMaxLengthOfColInArr(allWords, cols, rows, maxLengthArr);
-
     writeTableInFile(allWords, maxLengthArr, cols, rows, filepathOutput);
     free(maxLengthArr);
     freeArrInArr(data, rows);
@@ -306,34 +305,90 @@ bool testColumsQuantity(void)
     return true;
 }
 
-bool testOneElement(void)
-{ // проверим то что запишется в файл прям поэлементно
-    return true;
-}
-
 bool testCheckMaxLenght(void)
 {
+    char* data[100] = { 0 };
+    int rows = readFile(data, "src/CSVprettyPrinter/testInput1.csv");
+    int cols = quantityOfCols(data);
+    int words = rows * cols;
+    char** allWords = calloc(words, sizeof(char*));
+    arrWithAllTheWords(data, allWords, rows, words);
+    int* maxLengthArr = malloc(sizeof(int) * cols);
+    getMaxLengthOfColInArr(allWords, cols, rows, maxLengthArr);
+    int expected[5];
+    expected[0] = 9;
+    expected[1] = 6;
+    expected[2] = 8;
+    expected[3] = 7;
+    expected[4] = 6;
+
+    for (int i = 0; i < cols; i++) {
+        if (maxLengthArr[i] != expected[i]) {
+            free(maxLengthArr);
+            free(allWords);
+            return false;
+        }
+    }
+    free(maxLengthArr);
+    free(allWords);
+
     return true;
 }
 
-int tests()
+bool testSimpleOccasion(void)
 {
+    printf("Проверка простого случая...");
+    algorithm("src/CSVprettyPrinter/testInput2.csv", "src/CSVprettyPrinter/testOutput.txt");
+    char* expected[7] = { 0 };
+    expected[0] = "+====+========+========+\n";
+    expected[1] = "| ID | Name   | Active |\n";
+    expected[2] = "+====+========+========+\n";
+    expected[3] = "|  1 | Item A | true   |\n";
+    expected[4] = "+----+--------+--------+\n";
+    expected[5] = "|  2 | Item B | false  |\n";
+    expected[6] = "+----+--------+--------+\n";
+    FILE* file = fopen("src/CSVprettyPrinter/testOutput.txt", "r");
+    if (file == NULL) {
+        printf("file not found!\n");
+        return false;
+    }
+    char buffer[256];
+    int lineNum = 0;
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        if (lineNum >= 7) {
+            break;
+        }
+        if (strcmp(buffer, expected[lineNum]) != 0) {
+            fclose(file);
+            return false;
+        }
+        lineNum++;
+    }
+    fclose(file);
+    printf("завершена успешно!\n");
+    return true;
+}
 
+int tests(void)
+{
     if (!testRowsQuantity()) {
-        printf("с ошибкой в подстчете кол-ва строк");
+        printf("с ошибкой в подстчете кол-ва строк\n");
         return 1;
     }
 
     if (!testColumsQuantity()) {
-        printf("с ошибкой в подсчете кол-ва столбцов");
+        printf("с ошибкой  в подсчете кол-ва столбцов\n");
+        return 1;
+    }
+    if (!testSimpleOccasion()) {
+        printf("с ошибкой  в тестировании простого случая\n");
         return 1;
     }
 
-    /*
-    if (!testOneElement()) {
-        printf("g");
+    if (!testCheckMaxLenght()) {
+        printf("с ошибкой в тестировании полученных максимальных длин столбцов\n");
         return 1;
-    }*/
+    }
 
     printf("Все тесты пройдены успешно!\n");
     return 0;
