082dcd2e63e50d402ff0c07d3a352667f0a04b24
Yana Kalsina <yaprogrammer18@gmail.com>
2026-04-24 11:41:11 +0300

    Вынесла метод loadBase в main

--- src/flightCheckIn/tests.c
+++ src/flightCheckIn/tests.c
@@ -2,6 +2,39 @@
 #include <assert.h>
 #include <string.h>
 
+// функция загрузки дерева
+AVL* loadBase(const char* filename)
+{
+    AVL* tree = avlCreate();
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Файл не найден\n");
+        avlFree(tree);
+        return NULL;
+    }
+    char buffer[100] = { 0 };
+    while ((fgets(buffer, sizeof(buffer), file) != NULL)) {
+        size_t indexOfLineBreak = strcspn(buffer, "\n");
+        if (indexOfLineBreak < sizeof(buffer)) {
+            buffer[indexOfLineBreak] = '\0';
+        } else {
+            buffer[sizeof(buffer) - 1] = '\0';
+        }
+        char* colon = strchr(buffer, ':');
+        if (colon == NULL) {
+            continue;
+        }
+        *colon = '\0';
+        char* code = buffer;
+        char* name = colon + 1;
+        avlPush(tree, code, name);
+    }
+    fclose(file);
+    int qty = avlSize(tree);
+    printf("Загружено %d аэропортов. Система готова к работе.\n", qty);
+    return tree;
+}
+
 void testCreateAndFree(void)
 {
     AVL* tree = avlCreate();
